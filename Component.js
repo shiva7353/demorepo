9600c1fa7bff9fb5cfcfd496af053b2832010e99
shiva7353 <shivarajgowda321@gmail.com>
2021-01-23 14:55:03 +0530

    First commit

---
+++ Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(['sap/suite/ui/generic/template/lib/AppComponent'], function(AppComponent) {
+    return AppComponent.extend("listreport.listreportusingnothwind29dec20.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
---
+++ controller/listReportExt.controller.js
@@ -0,0 +1,36 @@
+sap.ui.define([], function () {
+    return {
+        onBeforeRebindTableExtension: function (oEvent) {
+            debugger;
+            var oBindingParams = oEvent.getParameter("bindingParams");
+
+
+            //   var oSmartTable = oEvent.getSource();
+            //   var oSmartFilterBar = this.byId(oSmartTable.getSmartFilterId());
+
+            //       var oCustomControl = oSmartFilterBar.getControlByKey("CustomDateFilter");
+            //       var oDateValue = oCustomControl.getDateValue(); //give input like this --  Jul 16, 1996, 5:30:00 AM
+            //       if (oDateValue) {
+            //        oBindingParams.filters.push(new sap.ui.model.Filter("ShippedDate", "EQ", oDateValue));
+            //      }
+
+            var oSmartTable = oEvent.getSource();
+            var oSmartFilterBar = this.byId(oSmartTable.getSmartFilterId());
+            var oCustomControl = oSmartFilterBar.getControlByKey("customShipCity");
+            var sShipCity = oCustomControl.getValue();
+            if (sShipCity) {
+                oBindingParams.filters.push(new sap.ui.model.Filter("ShipCity", "EQ", sShipCity));
+            }
+        },
+        onInitSmartFilterBarExtension: function (oEvent) {
+            debugger;
+        },
+        symanticContry: function (sValue) {
+            if (sValue > 50) {
+                return "Success";
+            } else {
+                return "Error";
+            }
+        }
+    }
+});
